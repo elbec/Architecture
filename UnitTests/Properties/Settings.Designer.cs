7d726b2226ff9f777369e4c000e807f42f6b0d20
elbec <elbe.christopher@gmail.com>
2020-07-09 19:06:19 +0200

    added ui tests

--- UnitTests/Properties/Settings.Designer.cs
+++ UnitTests/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Architecture.Properties {
+namespace UnitTests.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
@@ -20,6 +20,17 @@ namespace Architecture.Properties {
         public static Settings Default {
             get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\COCO\\Work\\MyTestSQL.mdf;In" +
+            "tegrated Security=True;Connect Timeout=30")]
+        public string sqlConnectionString {
+            get {
+                return ((string)(this["sqlConnectionString"]));
             }
         }
     }
---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        public static Architecture.SQL Arch = new Architecture.SQL();
+        public static DataTable dt = new DataTable();
+        public static string connectionString = UnitTests.Properties.Settings.Default.sqlConnectionString;
+
+        [TestMethod]
+        public void TestConnectionString()
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(connectionString);
+            }
+            catch
+            {
+                Assert.Fail("Connection Failed!");
+            }
+        }
+
+        [TestMethod]
+        public void TestLoadDataTable()
+        {
+            DataTable dt1 = new DataTable();
+            Arch.LoadDataTable(ref dt1, connectionString, "Select * FROM TestTable");
+
+            Assert.IsTrue(dt1.Rows.Count > 0, "Cannot load DataTable");
+        }
+    }
+}
+
